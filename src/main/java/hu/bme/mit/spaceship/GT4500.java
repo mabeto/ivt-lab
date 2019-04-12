388198d2795a64f6bcad2f2ea602ea28d8202084
Bence <mabeto1996@gmail.com>
2019-04-12 09:14:10 +0200

    travis

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -87,7 +87,7 @@ public class GT4500 implements SpaceShip {
               second = secondaryTorpedoStore.fire(1);
               wasPrimaryFiredLast = false;
             }
-		firingSucces = first && second
+		firingSucces = first && second;
         break;
     }
 
