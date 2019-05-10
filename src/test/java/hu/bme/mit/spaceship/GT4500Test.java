7796af1cff9bab041fec760f9a4062b247e24843
mabeto <mabeto1996@gmail.com>
2019-05-10 09:20:59 +0200

    Izoláció

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -7,28 +7,45 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
-
-  private GT4500 ship;
+    private TorpedoStore primary;
+	private TorpedoStore secondary;
+    private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+	primary = mock(TorpedoStore.class);
+	secondary= mock(TorpedoStore.class);
+    this.ship = new GT4500(primary,secondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+	when(primary.getTorpedoCount()).thenReturn(10);
+	when(secondary.getTorpedoCount()).thenReturn(10);
+	when(primary.isEmpty()).thenReturn(false);
+	when(secondary.isEmpty()).thenReturn(false);
+	when(primary.fire(1)).thenReturn(true);
+	when(secondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
+	verify(primary, times(1)).fire(1);
+	verify(secondary, times(0)).fire(1);
     assertEquals(true, result);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+	when(primary.getTorpedoCount()).thenReturn(10);
+	when(secondary.getTorpedoCount()).thenReturn(10);
+	when(primary.isEmpty()).thenReturn(false);
+	when(secondary.isEmpty()).thenReturn(false);
+	when(primary.fire(1)).thenReturn(true);
+	when(secondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
